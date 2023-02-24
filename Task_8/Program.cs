03f265c4c631867b133307a47ed78928d43dfe4c
Yasha <yashasidin333@yandex.ru>
2023-02-24 17:44:36 +0300

    Add solve of Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task_8: напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.Write("Введите число: ");
+int range = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write($"Четные числа в диапозоне от 1 до {range}: ");
+while (i < range - 1)
+{
+    if (i % 2 ==0)
+    {
+        Console.Write($"{i}, ");
+    }
+    i += 1;
+}
+while (i <= range)
+{
+    if (i % 2 ==0)
+    {
+        Console.Write($"{i}");
+    }
+    i += 1;
+}
